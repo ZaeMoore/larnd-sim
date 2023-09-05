333464102e2cbe0b80fa5011d4a0b6e7541355c1
Angela White <123964490+AWh1t3@users.noreply.github.com>
2023-09-05 09:46:47 -0500

    Update quenching.py
    Added data mode where recombination constant = 1

--- larndsim/quenching.py
+++ larndsim/quenching.py
@@ -34,6 +34,8 @@ def quench(tracks, mode):
         elif mode == physics.BIRKS:
             # Amoruso, et al NIM A 523 (2004) 275
             recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * dEdx / (detector.E_FIELD * detector.LAR_DENSITY))
+        elif mode == physics.DATA:
+            recomb = 1
         else:
             raise ValueError("Invalid recombination mode: must be 'physics.BOX' or 'physics.BIRKS'")
 
